835f4c4029fa8bbc73023756b944edeecb58b552
leonhpb <leonhpb@gmail.com>
2025-04-16 21:10:22 +0200

    Initial Commit

---
+++ bThermometer/bThermometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  bThermometerApp.swift
+//  bThermometer
+//
+//  Created by Leon Brum on 16.04.25.
+//
+
+import SwiftUI
+
+@main
+struct bThermometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bThermometerTests/bThermometerTests.swift
@@ -0,0 +1,17 @@
+//
+//  bThermometerTests.swift
+//  bThermometerTests
+//
+//  Created by Leon Brum on 16.04.25.
+//
+
+import Testing
+@testable import bThermometer
+
+struct bThermometerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
